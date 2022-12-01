5dd7672fe29a5ce4af08c30a8571258042f16534
AndreiLhu <leahu.andreilucian@gmail.com>
2022-12-01 17:48:53 +0100

    adding currency page, not styled

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import City from "./components/city/City";
 import Header from "./components/header/Header";
 import AboutUs from "./pages/AboutUs";
-import Footer from "./components/footer/Footer"
+import Footer from "./components/footer/Footer";
+import CurrencyConverter from "./components/currencyConverter";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/City" element={<City />} />
           <Route path="/About Us" element={<AboutUs />} />
+          <Route path="/Currency" element={<CurrencyConverter />} />
         </Routes>
       </BrowserRouter>
       <Footer />
-
     </div>
   );
 }

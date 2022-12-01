c5d564041e61766a9bf01bb928ef44d2b032fa91
stefan <stefan@stefan.com>
2022-12-01 18:26:03 +0100

    modal basic style

--- src/components/city-modal/index.js
+++ src/components/city-modal/index.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
+import "./app.css";
 
 function Modal({ imgLink, children, city, onClick }) {
   const [isOpen, setIsOpen] = useState(false);
 
-  if (!isOpen) return <button onClick={() => setIsOpen(true)}> open </button>;
+  if (!isOpen)
+    return (
+      <button className="open-btn" onClick={() => setIsOpen(true)}>
+        {" "}
+        open{" "}
+      </button>
+    );
   return (
-    <div className="overlay" onClick={onClick}>
+    <div className="overlay" onClick={() => setIsOpen(false)}>
       <div className="modal">
-        <button onClick={() => setIsOpen(false)}> close </button>
-        <div className="modal-content">
-          <img src={imgLink} alt="city" className="modal-image" />
-          <h3 className="modal-title">{city}</h3>
-          {children}
-        </div>
+        <button className="close-btn" onClick={() => setIsOpen(false)}>
+          {" "}
+          close{" "}
+        </button>
+
+        <img src={imgLink} alt="city" className="modal-image" />
+        <h3 className="modal-title">{city}</h3>
+        {children}
       </div>
     </div>
   );
